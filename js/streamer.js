83d28b97d1b9c32a82f7cb26ffe622e616b8fddd
Nilay <nilayanand@gmail.com>
2018-09-27 15:08:51 +0530

    Test file to check

--- js/streamer.js
+++ js/streamer.js
@@ -5,6 +5,27 @@ function init(){
     localVideo = document.getElementById("localVideo");
 }
 
+function createFbVideoStream(userId){
+  FB.api('/' + userId + '/live_videos', "POST", function(response) {
+    console.log("Rtmp url fetch sucessfull. " + response.stream_url);
+    // we have received rtmp url from facebook.
+    // lets start streaming
+    start(response.stream_url);
+  });
+}
+
+function fbLogin(){
+  FB.login(function(response) {
+    if (response.authResponse) {
+      console.log("Login sucessfull. Fetch rtmp url from facebook");
+      createFbVideoStream(response.authResponse.userID)
+    } else {
+     console.log('User cancelled login or did not fully authorize.');
+    }
+  }, {scope: 'publish_video,email'});
+}
+
+
 function start(rtmpUrl) {
     Flashphoner.createSession({urlServer: "wss://34.199.108.19:8443"}).on(Flashphoner.constants.SESSION_STATUS.ESTABLISHED, function (session) {
         //session connected, start streaming
@@ -17,7 +38,7 @@ function start(rtmpUrl) {
 }
 
 function startStreaming(session, rtmpUrl) {
-    var streamName = "s2233"
+    var streamName = "s2234"
     session.createStream({
         name: streamName,
         display: localVideo,
